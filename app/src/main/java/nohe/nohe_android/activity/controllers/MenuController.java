9436b47bff3af7b00fc509431e0767b6c238bb1a
aloushek <aloushek@outlook.com>
2018-07-31 01:27:25 +0200

    Small fixes + icons.

--- app/src/main/java/nohe/nohe_android/activity/controllers/MenuController.java
+++ app/src/main/java/nohe/nohe_android/activity/controllers/MenuController.java
@@ -17,6 +17,7 @@ public class MenuController {
     public void setMenuTexts() {
         if (navigationView != null) {
             Menu menu = navigationView.getMenu();
+            menu.findItem(R.id.nav_user_name).setTitle(loginService.getUserFirstName() + " " + loginService.getUserSurname());
             menu.findItem(R.id.nav_name).setTitle(loginService.getUserFirstName());
             menu.findItem(R.id.nav_surname).setTitle(loginService.getUserSurname());
         }
